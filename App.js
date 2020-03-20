3d688570163bb5a3ad6fefa9bd9fa8921158575d
ncooney94 <nick.cooney@gmail.com>
2020-03-20 16:08:01 +1100

    first push

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+import Home from './screens/home';
+
+const getFonts = () => 
+  Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded) {
+    return (
+      <Home />
+    )
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    )
+  }
+
+}
+
+
